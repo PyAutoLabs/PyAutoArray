51121f77a16bc3492d3c75d17e75b2271cf2c624
Jammy2211 <JNightingale2211@gmail.com>
2026-06-25 14:44:12 +0100

    Restore arcsec tick config in tests

--- test_autoarray/plot/test_utils.py
+++ test_autoarray/plot/test_utils.py
@@ -4,10 +4,15 @@ from autoarray.plot.utils import _arcsec_labels
 
 
 def test_arcsec_labels_default_suffix_format():
-    conf.instance["visualize"]["general"]["ticks"]["symbol_over_decimal"] = False
+    ticks = conf.instance["visualize"]["general"]["ticks"]
+    original = ticks.get("symbol_over_decimal", False)
+    try:
+        ticks["symbol_over_decimal"] = False
 
-    assert _arcsec_labels([-1.0, 0.0, 1.0]) == ['-1"', '0"', '1"']
-    assert _arcsec_labels([3.8]) == ['3.8"']
+        assert _arcsec_labels([-1.0, 0.0, 1.0]) == ['-1"', '0"', '1"']
+        assert _arcsec_labels([3.8]) == ['3.8"']
+    finally:
+        ticks["symbol_over_decimal"] = original
 
 
 def test_arcsec_labels_symbol_over_decimal():
